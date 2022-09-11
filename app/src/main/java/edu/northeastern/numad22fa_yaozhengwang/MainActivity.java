301377cdad213ef0c891f2c8c8c345edaf6bb9e9
soander <Wang-yaozheng@outlook.com>
2022-09-11 01:00:18 -0700

    The first week assignment.

--- app/src/main/java/edu/northeastern/numad22fa_yaozhengwang/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_yaozhengwang/MainActivity.java
@@ -3,6 +3,8 @@ package edu.northeastern.numad22fa_yaozhengwang;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button aboutMe = findViewById(R.id.aboutMe);
+        aboutMe.setOnClickListener(view -> Toast.makeText(
+                MainActivity.this,
+                "Name: Yaozheng Wang\n" +
+                        "Email: wang.yaozh@northeastern.edu",
+                Toast.LENGTH_LONG).show()
+        );
     }
+
+
 }
